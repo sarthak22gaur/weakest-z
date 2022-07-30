932adf9977e44ab0ce9162229a7ba11e688b7418
sarthakgaur22 <sarthakgaur22@gmail.com>
2022-07-29 23:17:31 -0700

    init setup and db populate

---
+++ scripts/db-populate.ts
@@ -0,0 +1,31 @@
+import { prisma } from "../src/server/db/client";
+import Papa from "papaparse";
+
+const file = "./dbz.csv";
+import { readFileSync } from "fs";
+
+
+const content = readFileSync(file, "utf8");
+const populate = async () => {
+  Papa.parse(content, {
+    header: true,
+    delimiter: "\n",
+    complete: async (results) => {
+      // let dbData = [];
+      let index = 0;
+      const data = results.data;
+
+      const formattedPokemon = results.data.map((p, index) => ({
+        id: index + 1,
+        name: (p as { Name: string }).Name,
+        img_url: (p as { ImageURL: string }).ImageURL,
+      }));
+      const created = await prisma.dbz.createMany({
+        data: formattedPokemon,
+      });
+      console.log(created);
+
+    },
+  });
+};
+populate();
