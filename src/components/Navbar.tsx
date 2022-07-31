be3290384df2c564f1a9a7cd9ce386a9ccc32f36
sarthakgaur22 <sarthakgaur22@gmail.com>
2022-07-30 18:18:06 -0700

    navbar update

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,19 +5,18 @@ const Navbar = () => {
   return (
     <>
       <div className="flex w-full gap-8 px-8 py-2 justify-between bg-orange-500">
-        <Image
-          className=""
-          width="150px"
-          height="20px"
-          src="/dbz.png"
-          alt=""
-        />
+        <Link href="/">
+          <div className="flex cursor-pointer">
+            <Image
+              className=""
+              width="150px"
+              height="50px"
+              src="/dbz.png"
+              alt=""
+            />
+          </div>
+        </Link>
         <ul className="flex w-full gap-8 px-8 justify-end items-center">
-          <li>
-            <h1 className="text-2xl font-medium p-4">
-              <Link href="/">Vote</Link>
-            </h1>
-          </li>
           <li className="border-2 border-white p-2 m-2 hover:text-orange-500 hover:bg-white rounded">
             <h1 className="text-2xl font-medium ">
               <Link href="/results">Results</Link>
