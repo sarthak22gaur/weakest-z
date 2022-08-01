fa9d076bb86d6a11a2ad44a4d217e4bb4a3d5fd6
sarthakgaur22 <sarthakgaur22@gmail.com>
2022-07-31 22:50:06 -0700

    updated about

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,4 +1,5 @@
 import Navbar from "../components/Navbar";
+import { FaGithub } from "react-icons/fa";
 
 export default function AboutPage() {
   return (
@@ -6,41 +7,48 @@ export default function AboutPage() {
       <Navbar />
       {/* <div className="relative"> */}
       <div className="flex justify-center">
-      <div className="flex flex-col items-center max-w-lg text-justify leading-9 text-gray-400 text-2xl m-4 p-4">
-        <div className="">
-          <h1 className="font-bold text-3xl">Why?</h1>
-          <p className="pt-4 text-white">
-            Cuz everyone knows Chichi is the strongest, so it was time to find
-            out who is the weakest.
-          </p>
-        </div>
-        <div>
-          <h1 className="text-xl mt-12 font-bold text-gray-400">
-            This is made with the t3 stack(trpc, tailwind typescript)
-            bootstraped by create-t3-app
-          </h1>
-          <p className="text-lg">
-            Learn more at 👉🏻{" "}
-            <a
-              className="text-cyan-500 font-semibold"
-              href="https://init.tips/"
-            >
-              init.tips
-            </a>
-          </p>
-        </div>
-        <div className="mt-16 text-lg">
-          <p>
-            Inspired from Theo&apos;s roundest pokemon polling site that he
-            built during one of his livestreams👉🏻{" "}
-            <span className="font-bold tracking-wide">
-              <a className="text-cyan-500" href="https://roundest.t3.gg/">
-                roundest.t3.gg
+        <div className="flex flex-col items-center max-w-lg text-justify leading-9 text-gray-400 text-2xl m-4 p-4">
+          <div className="">
+            <h1 className="font-bold text-3xl">Why?</h1>
+            <p className="pt-4 text-white">
+              Cuz everyone knows Chichi is the strongest, so it was time to find
+              out who is the weakest.
+            </p>
+            <span className="text-xl flex items-center gap-4 my-4">
+              Find source code here 👉🏻
+              <a className="text-cyan-500" href="https://github.com/sarthak22gaur/weakest-z">
+                <FaGithub size={22} />
               </a>
             </span>
-          </p>
+          </div>
+          <div>
+            <h1 className="text-xl mt-8 py-4 font-bold text-gray-400">
+              This is made with the t3 stack(trpc, tailwind typescript)
+              bootstraped by create-t3-app
+            </h1>
+
+            <p className="text-lg">
+              Learn more at 👉🏻{" "}
+              <a
+                className="text-cyan-500 font-semibold"
+                href="https://init.tips/"
+              >
+                init.tips
+              </a>
+            </p>
+          </div>
+          <div className="mt-16 text-lg">
+            <p>
+              Inspired from Theo&apos;s roundest pokemon polling site that he
+              built during one of his livestreams👉🏻{" "}
+              <span className="font-bold tracking-wide">
+                <a className="text-cyan-500" href="https://roundest.t3.gg/">
+                  roundest.t3.gg
+                </a>
+              </span>
+            </p>
+          </div>
         </div>
-      </div>
       </div>
       {/* </div> */}
     </>
