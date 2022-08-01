3805abf6636275ccc5472b6b0b6176b55a40b8f2
sarthakgaur22 <sarthakgaur22@gmail.com>
2022-07-31 22:41:00 -0700

    final for now

--- README.md
+++ README.md
@@ -1,7 +1,14 @@
 # Weakest
-This is inspired by Theo's roundest pokemon site (roundest.t3.gg) that he built during one of his streams.
-I took this up to get a feel for the t3 stack (also put together by Theo)
+Cuz everyone knows Chichi is the strongest, so it was time to find out who is the weakest.
+
+## Data
+The list along with the images was scraped from MyAnimeList's DBZ page using a Golang scraper.
+
+## Inspiration
+This is inspired by Theo's roundest pokemon site ([roundest.t3.gg](https://roundest.t3.gg/)) that he built during one of his streams.
+I took this up to get a feel for the t3 stack (also put together by Theo). Resource at [init.tips](https://init.tips/)
 
 ## TODO
-1. Add options for multiple animes
-2. Optimize for better SSG and caching of results
+1. Add initial loading animation
+2. Add options for multiple animes
+3. Optimize for better SSG and caching of results
--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,15 +1,48 @@
 import Navbar from "../components/Navbar";
 
-export default function AboutPage(  ) {
-    return (
-      <>
-        <Navbar />
-        {/* <div className="relative"> */}
-          <h1 className="text-3xl p-4 font-bold text-center">Coming Soon 😅</h1>
-          <div className="flex justify-center items-center">
-          </div>
-        {/* </div> */}
-      </>
-    );
-  }
-  
+export default function AboutPage() {
+  return (
+    <>
+      <Navbar />
+      {/* <div className="relative"> */}
+      <div className="flex justify-center">
+      <div className="flex flex-col items-center max-w-lg text-justify leading-9 text-gray-400 text-2xl m-4 p-4">
+        <div className="">
+          <h1 className="font-bold text-3xl">Why?</h1>
+          <p className="pt-4 text-white">
+            Cuz everyone knows Chichi is the strongest, so it was time to find
+            out who is the weakest.
+          </p>
+        </div>
+        <div>
+          <h1 className="text-xl mt-12 font-bold text-gray-400">
+            This is made with the t3 stack(trpc, tailwind typescript)
+            bootstraped by create-t3-app
+          </h1>
+          <p className="text-lg">
+            Learn more at 👉🏻{" "}
+            <a
+              className="text-cyan-500 font-semibold"
+              href="https://init.tips/"
+            >
+              init.tips
+            </a>
+          </p>
+        </div>
+        <div className="mt-16 text-lg">
+          <p>
+            Inspired from Theo&apos;s roundest pokemon polling site that he
+            built during one of his livestreams👉🏻{" "}
+            <span className="font-bold tracking-wide">
+              <a className="text-cyan-500" href="https://roundest.t3.gg/">
+                roundest.t3.gg
+              </a>
+            </span>
+          </p>
+        </div>
+      </div>
+      </div>
+      {/* </div> */}
+    </>
+  );
+}
